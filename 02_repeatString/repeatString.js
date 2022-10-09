ff9bd6f3558b0d19196d20346d8b83ab74335127
luis <luisdavid2014@gmail.com>
2022-10-09 14:36:14 +0200

    exercises from 1 to 4 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if (num < 0){
+        return 'ERROR'
+    }
+    let multi = '';
+    for (let i= 0;i < num;i++){
+        multi += str;
+    }
+    return multi
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    for (let i = 0; i<arg.length; i++){
 
+        if (arr.includes(arg[i])){
+            let index = arr.indexOf(arg[i]);
+            arr.splice(index,1);
+        }
+    
+        
+        
+    }
+    return arr
 };
 
 // Do not edit below this line
