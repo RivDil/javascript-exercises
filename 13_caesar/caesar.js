6ffa515fdbfc4f34573ce0f07eade9ab0b8f43e7
luis <luisdavid2014@gmail.com>
2022-10-11 12:10:45 +0200

    all exercises done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,36 @@
-const caesar = function() {
-
+const caesar = function(str,num) {
+    let regex = (/\w/g);
+    let strDivided = str.split('');
+    let arr = [];
+    for (let i = 0; i < strDivided.length; i++){
+        if (strDivided[i].match(regex)){ 
+            let code = strDivided[i].charCodeAt()
+            debugger
+            if (code >= 65 && code <= 90){
+                let sum = code + (num%26);
+                if (sum < 65){
+                    sum += 26;
+                    arr.push(String.fromCharCode(sum))
+                }else if (sum > 90){
+                    sum = sum-26;
+                    arr.push(String.fromCharCode(sum))
+                }else arr.push(String.fromCharCode(sum))
+            }
+            if (code >= 97 && code <= 122){
+                let sum = code + (num%26);
+                if (sum < 97){
+                    sum += 26;
+                    arr.push(String.fromCharCode(sum))
+                }else if (sum > 122){
+                    sum =sum- 26;
+                    arr.push(String.fromCharCode(sum))
+                }else arr.push(String.fromCharCode(sum))
+            }
+        }else{
+            arr.push(str[i])
+        }
+    }
+    return arr.join('')
 };
 
 // Do not edit below this line
